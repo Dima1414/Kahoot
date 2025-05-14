60b0b9b52867e786eb1b539cf748146315ef486c
dima14 <816182@seq.org>
2025-05-13 22:08:50 -0700

    add second question

--- src/main/java/com/example/KahootProject/PersonController.java
+++ src/main/java/com/example/KahootProject/PersonController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
-import java.util.Arrays;
-
 @Controller
 public class PersonController {
 
