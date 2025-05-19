66ff4acb28a1466ba8fa56e7343126d86edfe317
dima14 <816182@seq.org>
2025-05-18 17:13:41 -0700

    functional buttons and scorekeeping

--- src/main/java/com/example/KahootProject/PersonController.java
+++ src/main/java/com/example/KahootProject/PersonController.java
@@ -3,9 +3,14 @@ package com.example.KahootProject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PersonController {
+
+    private int scoreCount = 0;
 
     @GetMapping
     String getPeople(Model model) {
@@ -18,7 +23,22 @@ public class PersonController {
          */
 
 
-        model.addAttribute("question1", g.getQuestionSet().get((int)(Math.random() * 3)));
+        model.addAttribute("questionRand", g.getQuestionSet().get((int)(Math.random() * 3)));
+
+        return "people";
+    }
+    @PostMapping("/answer")
+    String answer(@RequestParam String answerString, @RequestParam String trueAnswer, Model model) {
+        Game g = new Game();
+        int rand = (int)(Math.random() * 3);
+        if(trueAnswer.equals(answerString)) {
+            scoreCount++;
+        } else {
+            scoreCount = 0;
+        }
+        model.addAttribute("answer", answerString);
+        model.addAttribute("score", scoreCount);
+        model.addAttribute("questionRand", g.getQuestionSet().get(rand)); // next question, del .getChoices().get(rand)
 
         return "people";
     }
